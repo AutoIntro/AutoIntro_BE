c00838c8b13e20bae62b2673d3c19ef9627914e0
mybookG <gil0jin@gmail.com>
2026-04-07 22:54:52 +0900

    [feat] #1: 초기 세팅구현

---
+++ src/main/java/com/capstone/autointro/AutoIntroApplication.java
@@ -0,0 +1,12 @@
+package com.capstone.autointro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoIntroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoIntroApplication.class, args);
+    }
+}
---
+++ src/test/java/com/capstone/autointro/AutoIntroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.autointro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoIntroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
